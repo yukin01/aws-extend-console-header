0a2947af103e130a1a372e8262a7efee1cfa0a76
yukin01 <bump1105@gmail.com>
2020-05-16 15:11:42 +0900

    First commit

---
+++ webpack.config.ts
@@ -0,0 +1,47 @@
+import * as path from 'path'
+import { Configuration } from 'webpack'
+import HTMLWebpackPlugin from 'html-webpack-plugin'
+//@ts-ignore
+import CopyPlugin from 'copy-webpack-plugin'
+
+const config: Configuration = {
+  entry: {
+    popup: path.join(__dirname, 'src/popup.ts'),
+    contentScripts: path.join(__dirname, 'src/contentScripts.ts')
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+            compilerOptions: { module: 'esnext' }
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx', '.json']
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.join(__dirname, 'src/popup.html'),
+      filename: 'popup.html',
+      chunks: ['popup']
+    }),
+    new CopyPlugin({patterns: [{ from: 'public', to: '.' }]})
+  ]
+}
+
+export default config
